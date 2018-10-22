f9bae4dfbdf9bd0f02c8d6fbe32bce7c32121053
Paul Nagle <paulnagle@gmail.com>
2018-10-22 21:27:24 +0100

    removed a comma

--- js/map.js
+++ js/map.js
@@ -139,7 +139,7 @@ const tomato_map_search = function($) {
 
       var listContent = "<tr  id='" + resultID + "' >";
       listContent += "<td>" + val.start_time.substring(0, 5)  + "</td>";
-      listContent += "<td><b>" + val.meeting_name + ", </b>";
+      listContent += "<td><b>" + val.meeting_name + "</b>";
       if (val.location_text)            { listContent += val.location_text ; }
       if (val.location_street)          { listContent += ", " + val.location_street; }
       if (val.location_info)            { listContent += ", " +  val.location_info; }
